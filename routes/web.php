798bcde74a355aa752236d6c1726d8f5efa219a8
JulianMSouza <julian.projetos@gmail.com>
2022-06-17 15:37:58 -0300

    Criada store para armazenamento de dados em banco. Criada diretiva para tratar bloqueio de requisição na view. Criada rota de post para inserção de dados em base.

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ use App\Http\Controllers\EventController;
 Route::get('/', [EventController::class, 'index']);
 //Nova rota para o método create da nova controller  EventController
 Route::get('/events/create', [EventController::class, 'create']);
+Route::post('/events', [EventController::class, 'store']);
 
 Route::get('/contact', function () {
     return view('contact');
